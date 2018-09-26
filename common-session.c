44222d5ffa445ad44c49dd28704132295bb94fa7
river <riverfor@gmail.com>
2018-09-26 00:36:30 -0400

    Update pw_dir to /home/qpython

--- common-session.c
+++ common-session.c
@@ -592,8 +592,8 @@ void fill_passwd(const char* username) {
 	}
 	ses.authstate.pw_uid = pw->pw_uid;
 	ses.authstate.pw_gid = pw->pw_gid;
-	ses.authstate.pw_name = "./"; //m_strdup(pw->pw_name);
-	ses.authstate.pw_dir = m_strdup(pw->pw_dir);
+	ses.authstate.pw_name = m_strdup(pw->pw_name);
+	ses.authstate.pw_dir = "/sdcard/qpython";//m_strdup(pw->pw_dir);
 	ses.authstate.pw_shell = m_strdup(pw->pw_shell);
 	{
 		char *passwd_crypt = pw->pw_passwd;
